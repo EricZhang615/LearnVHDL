ca7f5dabe3659ba632e9e0351389cb5d8967243e
EricZhang615 <eric20000615@gmail.com>
2020-10-09 22:41:21 +0800

    test crossover

--- crossover1000.vhd
+++ crossover1000.vhd
@@ -12,18 +12,18 @@ entity crossover1000 is							--define entity.
 end crossover1000;
 
 architecture crossover1000_arch of crossover1000 is	--define architecture
-
+signal clk_tmp: STD_LOGIC:='1';
 begin
    process(clk,rst)
 	 variable c: integer range 0 to 499:= 0;
  	 begin
 		if rst = '1' then
-			clkout <= '1';
+			clk_tmp <= '1';
 			c:=0;
 		else
 		if rising_edge(clk) then
  			if c=499 then
- 				clkout <= not clkout;
+ 				clk_tmp <= not clk_tmp;
  				c:=0;
  			else
  				c:=c+1;
@@ -31,4 +31,5 @@ begin
  		end if;
 	end if;
    end process;
+	clkout <= clk_tmp;
 end;
