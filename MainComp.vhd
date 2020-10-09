5fd1753b432676f83edca51c3efb4505e17db7e4
EricZhang615 <eric20000615@gmail.com>
2020-10-09 22:35:17 +0800

    test crossover

--- MainComp.vhd
+++ MainComp.vhd
@@ -1,1 +1,25 @@
 library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use IEEE.std_logic_arith.all;				--import libraries
+
+entity TrafficLightsControl is
+  port(
+  clk: in STD_LOGIC;
+  rst: in STD_LOGIC;
+  test_output: out STD_LOGIC
+  );
+end TrafficLightsControl;
+
+architecture TrafficLightsControl_arch of TrafficLightsControl is
+  component crossover1000
+    port(
+    clk: in STD_LOGIC;
+    rst: in STD_LOGIC;
+    clkout: out STD_LOGIC
+    );
+  end component;
+
+begin
+  crossover:crossover1000 port map(clk=>clk,rst=>rst,clkout=>test_output);
+end TrafficLightsControl_arch;
