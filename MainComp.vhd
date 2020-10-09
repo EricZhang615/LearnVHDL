242b2ae5528829e27e10e181adfa31b0433e2d0b
EricZhang615 <eric20000615@gmail.com>
2020-10-10 01:40:50 +0800

    加入核心计数器 待测试

--- MainComp.vhd
+++ MainComp.vhd
@@ -7,7 +7,8 @@ entity TrafficLightsControl is
   port(
   clk: in STD_LOGIC;
   rst: in STD_LOGIC;
-  test_output: out STD_LOGIC
+  TS: out STD_LOGIC_VECTOR(2 downto 0);
+  CS: out STD_LOGIC_VECTOR(5 downto 0)
   );
 end TrafficLightsControl;
 
@@ -20,6 +21,18 @@ architecture TrafficLightsControl_arch of TrafficLightsControl is
     );
   end component;
 
+  component counter
+    port(
+    clk: in STD_LOGIC;
+    rst: in STD_LOGIC;
+    TrafficState: out STD_LOGIC_VECTOR(2 downto 0);
+    CountSec: out STD_LOGIC_VECTOR(5 downto 0)
+    );
+  end component;
+
+  signal clk_1s: STD_LOGIC;
+
 begin
-  crossover:crossover1000 port map(clk=>clk,rst=>rst,clkout=>test_output);
+  crossover:crossover1000 port map(clk=>clk,rst=>rst,clkout=>clk_1s);
+  counter:counter port map(clk=>clk_1s,rst=>rst,TrafficState=>TS,CountSec=>CS);
 end TrafficLightsControl_arch;
