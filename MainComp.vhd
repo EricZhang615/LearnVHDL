2ea8793b58a7fc1a11c4188e78b21a1aa4158c5b
EricZhang615 <eric20000615@gmail.com>
2020-10-10 14:13:21 +0800

    MainComp加入点阵

--- MainComp.vhd
+++ MainComp.vhd
@@ -7,7 +7,9 @@ entity TrafficLightsControl is
   port(
   clk: in STD_LOGIC;
   rst: in STD_LOGIC;
-  TS: out STD_LOGIC_VECTOR(2 downto 0);
+  row: out STD_LOGIC_VECTOR(7 downto 0);
+  col_r: out STD_LOGIC_VECTOR(7 downto 0);	--output signal for selecting columns of red lights
+  col_g: out STD_LOGIC_VECTOR(7 downto 0)		--output signal for selecting columns of green lights
   CS: out STD_LOGIC_VECTOR(5 downto 0)
   );
 end TrafficLightsControl;
@@ -30,9 +32,21 @@ architecture TrafficLightsControl_arch of TrafficLightsControl is
     );
   end component;
 
+  component dotdisp
+    port(
+    clk: in STD_LOGIC;
+    TrafficState: in STD_LOGIC_VECTOR(2 downto 0);
+    row: out STD_LOGIC_VECTOR(7 downto 0);		--output signal for selecting and scanning rows of the displayer
+    col_r: out STD_LOGIC_VECTOR(7 downto 0);	--output signal for selecting columns of red lights
+    col_g: out STD_LOGIC_VECTOR(7 downto 0)		--output signal for selecting columns of green lights
+    );
+  end component;
+
   signal clk_1s: STD_LOGIC;
+  signal ts: STD_LOGIC_VECTOR(2 downto 0);
 
 begin
   crossover:crossover1000 port map(clk=>clk,rst=>rst,clkout=>clk_1s);
-  maincounter:counter port map(clk=>clk_1s,rst=>rst,TrafficState=>TS,CountSec=>CS);
+  maincounter:counter port map(clk=>clk_1s,rst=>rst,TrafficState=>ts,CountSec=>CS);
+  dot_array:dotdisp port map(clk=>clk,TrafficState=>ts,row=>row,col_r=>col_r,col_g=>col_g);
 end TrafficLightsControl_arch;
